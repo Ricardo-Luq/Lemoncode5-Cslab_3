1e98f2fad23cac52d2718c42c95812fea8ddbe69
Ricardo-Luq <luquemeridaricardo@gmail.com>
2024-04-23 13:24:44 +0200

    Uploaded task files

---
+++ Csharp-3-solution/cs3-app1-ReadBooks/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace cs3_app1_ReadBooks
+{
+    internal class Program
+    {
+
+        public class Book
+        {
+            public string Title { get; set; }
+            public bool IsRead { get; set; }
+        }
+        public static bool IsBookRead(Book[] books, string titleToSearch)
+        {
+            bool bookFound = false;
+            int i = 0;
+            while (!bookFound && i < books.Length)
+            {
+                if (titleToSearch == books[i].Title)
+                {
+                    return books[i].IsRead;
+                }
+                i++;
+            }
+            return false; //if no book was found, return false
+        }
+        static void Main(string[] args)
+        {
+            var books = new Book[] {
+                new Book
+                {
+                    Title = "Harry Potter y la piedra filosofal",
+                    IsRead = true
+                },
+                new Book
+                {
+                    Title = "Canción de hielo y fuego",
+                    IsRead = false
+                },
+                new Book
+                {
+                    Title = "Devastación",
+                    IsRead = true
+                },
+            };
+
+            Console.WriteLine(IsBookRead(books, "Devastación")); // true
+            Console.WriteLine(IsBookRead(books, "Canción de hielo y fuego")); // false
+            Console.WriteLine(IsBookRead(books, "Los Pilares de la Tierra")); // false
+        }
+    }
+}
---
+++ Csharp-3-solution/cs3-app2-SlotMachine/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace cs3_app2_SlotMachine
+{
+    internal class Program
+    {
+        public class SlotMachine
+        {
+            public string Title { get; set; }
+            public int Coins { get; set; }
+
+            public int play() {
+                this.Coins++;
+                Random rnd = new Random();
+
+                bool roll1 = rnd.Next(2) == 0;
+                bool roll2 = rnd.Next(2) == 0;
+                bool roll3 = rnd.Next(2) == 0;
+
+                Console.WriteLine($"Rolling...\n" +
+                    $"{(roll1 ? "GOOD" : "FAIL")}\n" +
+                    $"{(roll2 ? "GOOD" : "FAIL")}\n" +
+                    $"{(roll3 ? "GOOD" : "FAIL")}\n");
+                
+                if ( roll1 && roll2 && roll3 )
+                {
+                    int award = this.Coins;
+                    this.Coins = 0;
+                    return award;
+                }  else
+                {
+                    return -1;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            SlotMachine machine = new SlotMachine();
+            Console.WriteLine("Welcome! Would you like to test your luck!");
+            bool playing = true;
+            do
+            {
+                Console.WriteLine("[A] Insert one coin into the machine and play.");
+                Console.WriteLine("[B] Leave.");
+                string Answer = Console.ReadLine().ToUpper();
+                if ( Answer == "A")
+                {
+                    int result = machine.play();
+                    if (result > 0)
+                    {
+                        Console.WriteLine($"Congratulations!!!. You won {result} coins!!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Good luck next time!\n");
+                    }
+                } else
+                {
+                    playing = false;
+                }
+            } while (playing);
+        }
+    }
+}
